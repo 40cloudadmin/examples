d6be59547318a12054d16ae8741a1161a5577b92
Amir Naftali <amir@FortyCloud.local>
2017-01-02 14:02:16 +0200

    script updates+validatation

--- bash/install_gateway.sh
+++ bash/install_gateway.sh
@@ -24,18 +24,66 @@ PASSWORD=""
 TOKEN=""
 TARGET_GWSERVER=""
 SSH_KEY_FILE=
+#JQ=./jq-osx-amd64
+JQ=./jq-linux64
+#JQ=./jq-win64.exe
+
+validate_jq(){
+   which $JQ
+   if [ $? != 0 ]
+   then
+      echo "jq (bash json parser) is not available in current path"
+      echo "please download jq from https://stedolan.github.io/jq/ and make it available in your path" 
+      echo "make sure the JQ variable is pointing to the jq executable"
+      return 1
+   fi
+}
+validate_input(){
+   if [ -z "$USERNAME" ]
+   then
+      echo "please provide API username"
+      return 1
+   fi
+
+   if [ -z "$USERNAME" ]
+   then
+      echo "please provide API password"
+      return 1
+   fi
+   if [ -z "$TARGET_GWSERVER" ]
+   then
+      echo "please provide IP Address of the target server"
+      return 1
+   fi
+
+}
+usage(){
+   echo "script usage: $0 <api_username> <api_password> <target server IP>" 
+}
 main(){
     USERNAME=$1
     PASSWORD=$2   
     TARGET_GWSERVER=$3
+    validate_input 
+    if [ $? != 0 ]
+    then
+       usage
+       exit 1
+    fi
+    validate_jq
+    if [ $? != 0 ]
+    then
+       exit 1
+    fi
+
     JSONDATA="{\"auth\": {\"passwordCredentials\": {\"username\": \"$USERNAME\",\"password\": \"$PASSWORD\"},\"tenantName\": \"MyAccount\"}}"
     SESSION_RESPONSE=$(curl -s -X POST -d "$JSONDATA" https://$API_ENDPOINT/$RESOURCE_SESSION)
-    TOKEN=$(echo $SESSION_RESPONSE | ./jq-osx-amd64 '.access.token.id')
+    TOKEN=$(echo $SESSION_RESPONSE | $JQ '.access.token.id')
     TOKEN=$(echo $TOKEN | tr -d '"')
     echo "API SESSION TOKEN: "$TOKEN
     JSON_INSTALL_REQ="{\"installationPlatform\":\"$PLATFORM\",\"licenseName\":\"$LICENSE\"}"
     INSSCRIPT_RESPONSE=$(curl -s -H "X-Auth-Token:$TOKEN" -X POST -d "$JSON_INSTALL_REQ" https://$API_ENDPOINT/$RESOURCE_INSTALL_SCRIPT)
-    INSTALL_SCRIPT=$(echo $INSSCRIPT_RESPONSE | ./jq-osx-amd64 '.installationScript')
+    INSTALL_SCRIPT=$(echo $INSSCRIPT_RESPONSE | $JQ '.installationScript')
     INSTALL_SCRIPT=$(echo $INSTALL_SCRIPT | tr -d '"')
     ssh -i $SSH_KEY_FILE ubuntu@$TARGET_GWSERVER -T $INSTALL_SCRIPT
 }
